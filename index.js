da877a9fed64bb47020ce16d32ce5e83c634879b
Ben Marum <ben.j.marum@gmail.com>
2023-04-25 23:32:28 -0600

    fixed error

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // Code your solutions in this file
 
+const messages = [];
+function writeCards(names,event) {
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+console.log(messages)
+
+
 const int = 10;
 
 function countDown(int){
@@ -10,27 +21,3 @@ function countDown(int){
 }
 
 countDown(int);
-
-//------------------------------------------------------------------>
-/*
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-  }
-
-  return gifts;
-}
-
-const messages =[];
-
-function writeCards(names,event) {
-    for (let i = 0; i < names.length; i++) {
-      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-    }
-  
-    return messages;
-}
-
-writeCards(["Charlie", "Samip", "Ali"], "birthday");
-console.log(messages);
-*/
