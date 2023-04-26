76719dde3e2506e863701c21a906d08e4ee05d42
Ben Marum <ben.j.marum@gmail.com>
2023-04-25 23:01:03 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+
+const int = 10;
+
+function countDown(int){
+    while (int >= 0) {
+        console.log(int--);
+    }
+    return 
+}
+
+countDown(int);
+
+//------------------------------------------------------------------>
+/*
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+const messages =[];
+
+function writeCards(names,event) {
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return messages;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(messages);
+*/
